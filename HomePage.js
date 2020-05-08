1707faea10cbc6179d5260b262a8dd156b4f0343
Jpendy <Jpendy256@gmail.com>
2020-05-07 17:51:06 -0700

    trying to call api

--- HomePage.js
+++ HomePage.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import List from './List.js';
+import request from 'superagent';
 
 export default class HomePage extends Component {
 
     state ={
-        data: ''
+        data: []
+    }
+
+    async componentDidMount() {
+        let requestData = await request.get('https://thawing-coast-15109.herokuapp.com/animals')
+
+        this.setState( {data: requestData.body })
     }
 
 
@@ -13,9 +20,12 @@ export default class HomePage extends Component {
             <div>
                
                 <ul>
-                    <List listProp = {this.state.data}  />
+                    { this.state.data.map(item => {
+                        return <List listProp = { item }  />
+                    }) }
+                    
                 </ul>
-                
+
             </div>
         )
     }
